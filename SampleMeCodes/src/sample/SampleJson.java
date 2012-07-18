3504fbd6b64401bc134933ff5a288df4ac7a5a64
tnaka <nakagawa.takatoshi@tis.co.jp>
2012-07-18 13:07:15 +0900

    add

--- SampleMeCodes/src/sample/SampleJson.java
+++ SampleMeCodes/src/sample/SampleJson.java
@@ -2,10 +2,12 @@ package sample;
 
 import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -39,6 +41,11 @@ public class SampleJson {
 		map.put("eee", new Double(5.43));
 		map.put("fff", new int[]{1,2,3,4});
 		map.put("ggg", new Date());
+		List list = new ArrayList();
+		list.add("111");
+		list.add("222");
+		list.add("333");
+		map.put("hhh", list);
 		System.out.println(object2json(map));
 	}
 
@@ -54,14 +61,16 @@ public class SampleJson {
 		if (object != null) {
 			// key一覧取得
 			Iterator it = object.keySet().iterator();
-			// オブジェクトは、{(左の中括弧)で始まる
-			ret = "{";
 			// 初回フラグ
 			boolean flg = true;
 			// 名前用変数
 			Object string = null;
 			// 値用変数
 			Object value = null;
+			// 文字列
+			StringBuffer buffer = new StringBuffer();
+			// オブジェクトは、{(左の中括弧)で始まる
+			buffer.append("{");
 			// 名前数分ループ
 			while (it.hasNext()) {
 				// 初回判定
@@ -70,58 +79,23 @@ public class SampleJson {
 					flg = false;
 				} else {
 					// 「, 」で連結
-					ret += ", ";
+					buffer.append(", ");
 				}
 				// 名前取得
 				string = it.next();
 				// 値取得
 				value = object.get(string);
 				// 名前＋「: 」＋値で設定
-				ret += string2json(string.toString()) + ": " + value2json(value);
-				
+				buffer.append(string2json(string.toString()) + ": " + value2json(value));
 			}
-			// オブジェクトは、} (右の中括弧)で終る
-			ret += "}";
+			// オブジェクトは、} (右の中括弧)で終わる
+			buffer.append("}");
+			// 文字列を設定
+			ret = buffer.toString();
 		}
 		return ret;
 	}
 
-	/**
-	 * 文字列置換 {@link RestUtil#replaceAll(String, String, String, int)}
-	 * の開始位置を0としたもの
-	 * ※：CDCFPには、String#replaceAllが存在しないので、自作
-	 * 
-	 * @param string 対象文字列
-	 * @param target 置換元文字列
-	 * @param replace 置換後文字列
-	 * @return 置換後文字列
-	 */
-	private static String replaceAll(String string, String target, String replace) {
-		return replaceAll(string, target, replace, 0);
-	}
-
-	/**
-	 * 文字列置換
-	 * ※：CDCFPには、String#replaceAllが存在しないので、自作
-	 * 
-	 * @param string 対象文字列
-	 * @param target 置換元文字列
-	 * @param replace 置換後文字列
-	 * @param index 置換開始位置
-	 * @return 置換後文字列
-	 */
-	private static String replaceAll(String string, String target, String replace, int index) {
-		String ret = string;
-		if (ret != null && target != null && replace != null) {
-			int idx = ret.indexOf(target, index);
-			if (idx >= 0) {
-				ret = ret.substring(0, idx) + replace + ret.substring(idx + target.length());
-				ret = replaceAll(ret, target, replace, idx + replace.length());
-			}
-		}
-		return ret;
-	}
-	
 	/**
 	 * サニタイズ
 	 * 
@@ -174,31 +148,35 @@ public class SampleJson {
 			ret = object2json((Map) value);
 		} else if (value.getClass().isArray()) {
 			// 配列の場合
+			// 文字列
+			StringBuffer buffer = new StringBuffer();
 			// 配列は、[(左の大括弧)で始まる
-			ret = "[";
+			buffer.append("[");
 			// 配列数分ループ
 			for (int i = 0; i < Array.getLength(value); i++) {
 				// 初回判定
 				if (i != 0) {
 					// 値は、，(コンマ)で区切られる
-					ret += ", ";
+					buffer.append(", ");
 				}
 				// 値追加
-				ret += value2json(Array.get(value, i));
+				buffer.append(value2json(Array.get(value, i)));
 			}
 			// 配列は、](右の大括弧)で終わる
-			ret += "]";
+			buffer.append("]");
+			// 文字列をせて値
+			ret = buffer.toString();
 		} else if (value instanceof Collection) {
 			// java.util.Collectionも配列として対応
 			// Collectionを配列に変更して、再帰呼び出し
 			Collection collection = (Collection) value;
 			Object[] objects = new Object[collection.size()];
-			collection.toArray(objects);
+			objects = collection.toArray(objects);
 			ret = value2json(objects);
 		} else if (value instanceof Date) {
 			// 日付の場合
 			// JSONでは日付の指定がない為、フォーマットは独自
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			ret = string2json(sdf.format((Date) value));
 		} else {
 			// 文字列、その他はすべて文字列扱いとする
@@ -206,4 +184,41 @@ public class SampleJson {
 		}
 		return ret;
 	}
+	
+	/**
+	 * 文字列置換 {@link RestUtil#replaceAll(String, String, String, int)}
+	 * の開始位置を0としたもの
+	 * ※：CDCFPには、String#replaceAllが存在しないので、自作
+	 * 
+	 * @param string 対象文字列
+	 * @param target 置換元文字列
+	 * @param replace 置換後文字列
+	 * @return 置換後文字列
+	 */
+	private static String replaceAll(String string, String target, String replace) {
+		return replaceAll(string, target, replace, 0);
+	}
+
+	/**
+	 * 文字列置換
+	 * ※：CDCFPには、String#replaceAllが存在しないので、自作
+	 * 
+	 * @param string 対象文字列
+	 * @param target 置換元文字列
+	 * @param replace 置換後文字列
+	 * @param index 置換開始位置
+	 * @return 置換後文字列
+	 */
+	private static String replaceAll(String string, String target, String replace, int index) {
+		String ret = string;
+		if (ret != null && target != null && replace != null) {
+			int idx = ret.indexOf(target, index);
+			if (idx >= 0) {
+				ret = ret.substring(0, idx) + replace + ret.substring(idx + target.length());
+				ret = replaceAll(ret, target, replace, idx + replace.length());
+			}
+		}
+		return ret;
+	}
+
 }
