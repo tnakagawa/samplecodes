332e159df9e57e6c616086160f4a7e454b97abaa
tnaka <nakagawa.takatoshi@tis.co.jp>
2012-09-25 16:12:40 +0900

    test

--- SampleBundle/src/sample/bundle/Activator.java
+++ SampleBundle/src/sample/bundle/Activator.java
@@ -6,6 +6,7 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
+		// sample
 	}
 	
 	public void stop(BundleContext bundleContext) throws Exception {
